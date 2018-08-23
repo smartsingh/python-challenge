82c1fe33e67f7c75f72bafce9ac0491484157b2e
smartsingh <smartsingh@gmail.com>
2018-08-23 13:26:19 -0400

    updated to include method for finding average change. Starting the groundwork for finding greatest inc/dec

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,12 +1,22 @@
 import csv
 import os
 
-#establish goal parameters
+#establish goal parameters (i.e., total months, sum of revenue, etc)
 months_count = 0
 revenue = 0
+
+#for use with changes in between months
+current = 0
+previous = 0
+changes = []
+change_sum = 0
 avg_rev_delta = 0
+
+#for use with finding increase/decrease
 great_inc = 0
+great_inc_month = ""
 great_dec = 0
+great_dec_month = ""
 
 #use os path to join for compatability with other os'es (spelling?)
 csvpath = os.path.join("Resources","budget_data.csv")
@@ -18,9 +28,24 @@ with open(csvpath, newline="") as csvfile:
     csvheader = next(csvreader)
     
     for row in csvreader:
+        #count months
         months_count += 1
 
+        #sum revenue
         revenue += int(row[1])
+
+        #create list of changes to be used later
+        current = int(row[1])
+
+        changes.append(current - previous)
+
+        previous = int(row[1])
+
+    for row in range(1,len(changes)):
+        change_sum += changes[row]
+        
+    avg_rev_delta = round((change_sum / (months_count - 1)),2)
 
 print(months_count)
 print(revenue)
+print(avg_rev_delta)
